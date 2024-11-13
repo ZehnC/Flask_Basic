dc55f1d0a2bd3e697f0d92906d489224551be183
Maxwell Chen <91706650+ZehnC@users.noreply.github.com>
2024-11-13 11:11:09 -0600

    Updating __init__.py to return the an application

--- flask_board/board/__init__.py
+++ flask_board/board/__init__.py
@@ -1,10 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hellow World"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    return app
