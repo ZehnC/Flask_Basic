860396e85f7447fdfa071438dc694c6fa0b8b6fd
Maxwell Chen <91706650+ZehnC@users.noreply.github.com>
2024-11-13 15:29:29 -0600

    Reformat Home and About Pages to use Templates

--- flask_board/board/__init__.py
+++ flask_board/board/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 
+from board import pages
+
+#Creates Application (application factory)
 def create_app():
     app = Flask(__name__)
+
+    app.register_blueprint(pages.bp)
 
     return app
